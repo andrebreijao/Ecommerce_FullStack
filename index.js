188173f008b1904d7ef9931cb153840348d92019
andrebreijao <60301471+andrebreijao@users.noreply.github.com>
2021-02-18 10:06:15 -0300

    popup login
    adicionado a funcionadade de popup de login, falta a de cadastro

---
+++ index.js
@@ -0,0 +1,8 @@
+function togglePopup1(){
+    document.getElementById("popup-1").classList.toggle("active");
+  }
+      
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+  }
+      
