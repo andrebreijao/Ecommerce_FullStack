7b30cc06e93de5ef3b2e3711a96b08ce811283e1
andrebreijao <60301471+andrebreijao@users.noreply.github.com>
2021-02-21 18:52:33 -0300

    Ajuste de efeito visual
    sombra nos cards

--- interface_shopping.js
+++ interface_shopping.js
@@ -2,15 +2,11 @@ function rolar_esquerda() {
     let barra = document.getElementById("lista-categorias")
     let scroll = barra.scrollLeft - 100;
     barra.scroll(scroll, 0);
-    console.log(barra.scrollLeft);
 
 }
 
 function rolar_direita() {
     let barra = document.getElementById("lista-categorias")
     let scroll = barra.scrollLeft + 100;
-    barra.scroll(scroll, 0);
-    console.log(barra.scrollLeft);
-    console.log(barra.offsetWidth - barra.clientWidth);
-    console.log(barra.clientWidth);
+    barra.scroll(scroll, 0);  
 }
