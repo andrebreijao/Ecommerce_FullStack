805684b819fa281ce1c590ca17bbc654f7cc8bb4
andrebreijao <60301471+andrebreijao@users.noreply.github.com>
2021-02-25 13:16:42 -0300

    Interface lojas
    Falta deixar responsivo

--- lojas.js
+++ lojas.js
@@ -1,19 +1,19 @@
 document.querySelectorAll('.card-wishlist-vazio').forEach(item => {
     item.addEventListener('click', event => {
         console.log("vazio")
-        item.classList.remove('card-wishlist-vazio');
-      item.classList.add('card-wishlist-solido');
+        item.className = 'card-wishlist-solido';
     })
   })
 
 document.querySelectorAll('.card-wishlist-solido').forEach(item => {
 item.addEventListener('click', event => {
     console.log("solido")
-    item.classList.remove('card-wishlist-solido');
-    item.classList.add('card-wishlist-vazio');
+    item.className = 'card-wishlist-vazio';
 })
 })
 
-// function adicionar_remover_wishlist(){
-//     console.log(this.innerHTML)
-// }
+$(document).ready(function(){
+    $("#genero").click(function(){
+      $("#panel-genero").slideToggle("fast");
+    });
+  });
