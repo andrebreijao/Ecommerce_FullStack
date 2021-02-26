3d119f797a4d0a9b4fb0577fa610f74554b1d88f
andrebreijao <60301471+andrebreijao@users.noreply.github.com>
2021-02-26 13:59:44 -0300

    Interface lojas, ajuste banner com setas e timer

--- lojas.js
+++ lojas.js
@@ -1,3 +1,12 @@
+
+
+function redimensionar_body() {
+  let tamanho_tela = (screen.width-5).toString();
+  var x = document.getElementsByTagName("BODY")[0];
+  x.style.width = tamanho_tela + "px";
+  console.log(tamanho_tela)
+}
+
 document.querySelectorAll('.card-wishlist-vazio').forEach(item => {
     item.addEventListener('click', event => {
         console.log("vazio")
@@ -30,51 +39,69 @@ $(document).ready(function(){
 
 // Nao automático
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  clearInterval(my_timer);
+  my_timer = setInterval(proxslide, 3000);
+}
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+function proxslide() {
+  showSlides(slideIndex += 1);
+}
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-// }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// Automático
 
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
+
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+      slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM completamente carregado e analisado");
+  my_timer= setInterval(proxslide, 3000);
+
+});
+
+
+// Automático
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
+
+
