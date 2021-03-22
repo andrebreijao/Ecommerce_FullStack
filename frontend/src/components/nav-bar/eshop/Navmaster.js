54828c1abebe2d8762c937df0d9073286746a662
Sonia Carvalho <andrebreijao@gmail.com>
2021-03-22 15:37:26 -0300

    Ajuste pagina inicial React

--- frontend/src/components/nav-bar/eshop/Navmaster.js
+++ frontend/src/components/nav-bar/eshop/Navmaster.js
@@ -14,10 +14,10 @@ function Navmaster() {
     <div className="nav-bar">
       <img src="" alt="logo" id="logo-nav" />
       <div className="login">
-        <a href="#" id="cadastro" onClick={togglePopup2()}>
+        <a href="#" id="cadastro" onClick={togglePopup2}>
           <i className="fas fa-shopping-bag icone segundo-icone"></i>Sacola
         </a>
-        <a href="#" id="entrar" onClick={togglePopup1()} >
+        <a href="#" id="entrar" onClick={togglePopup1} >
           <i className="fa fa-user icone" ></i>
             Entrar
         </a>
