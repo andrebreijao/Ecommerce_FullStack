3f689cb9b6f825dcff73fd2a5c7f138be111f337
Sonia <andrebreijao@gmail.com>
2021-04-25 16:47:18 -0300

    Barra shoppings responsiva

--- frontend/src/components/nav-bar/shopping_valesul/NavValeSul2.js
+++ frontend/src/components/nav-bar/shopping_valesul/NavValeSul2.js
@@ -30,13 +30,15 @@ function Navvaleusul2() {
 
   return (
     <div className="nav-bar-shopping-loja2" id="nav-vale-sul-loja2">
-      <Link to="/">
-        <img
-          src="./Imagens/Shoppings/São_José_dos_Campos/Vale_Sul/Logo_shopping_Vale_SUL.jpg"
-          alt="logo"
-          id="logo-shopping"
-        />
-      </Link>
+      <div className="secao-logo-shop2">
+        <Link to="/">
+          <img
+            src="./Imagens/Shoppings/São_José_dos_Campos/Vale_Sul/Logo_shopping_Vale_SUL.jpg"
+            alt="logo"
+            id="logo-shopping2"
+          />
+        </Link>
+      </div>
       <div className="nav-bar-loja-menu2">
         <Link to="/">
           <li href="index.html">Home</li>
@@ -44,40 +46,42 @@ function Navvaleusul2() {
         <Link to="/valesul">
           <li>Shopping</li>
         </Link>
-        <Link to="/valesul">
+        {/* <Link to="/valesul">
           <li>Navegar Lojas</li>
-        </Link>
+  </Link> */}
       </div>
-      <div className="login2">
-        <Link id="cadastro" to="/carrinho">
-          <Badge
-            badgeContent={getTotalItems(cartItems)}
-            color="error"
-            style={{ color: 'white', position: 'relative', bottom: '5px' }}
-          >
-            <AddShoppingCartIcon />
-          </Badge>
-        </Link>
-        {userInfo ? (
-          <div className="loggedin2">
-            <NavDropdown title={userInfo.name.split(' ')[0]} id="username">
-              <LinkContainer to="/perfil" style={{ color: '#212529' }}>
-                <NavDropdown.Item>Perfil</NavDropdown.Item>
-              </LinkContainer>
-              <NavDropdown.Item
-                onClick={logoutHandler}
-                style={{ color: '#212529' }}
-              >
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          </div>
-        ) : (
-          <Link id="entrar" to="/login">
-            <i className="fa fa-user icone" />
-            Entrar
+      <div className="container-loggedin2">
+        <div className="login2">
+          <Link id="cadastro" to="/carrinho">
+            <Badge
+              badgeContent={getTotalItems(cartItems)}
+              color="error"
+              style={{ color: 'white', position: 'relative', bottom: '5px' }}
+            >
+              <AddShoppingCartIcon />
+            </Badge>
           </Link>
-        )}
+          {userInfo ? (
+            <div className="loggedin2">
+              <NavDropdown title={userInfo.name.split(' ')[0]} id="username">
+                <LinkContainer to="/perfil" style={{ color: '#212529' }}>
+                  <NavDropdown.Item>Perfil</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item
+                  onClick={logoutHandler}
+                  style={{ color: '#212529' }}
+                >
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </div>
+          ) : (
+            <Link id="entrar" to="/login">
+              <i className="fa fa-user icone" />
+              Entrar
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
