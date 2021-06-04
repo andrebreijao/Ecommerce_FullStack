f0ef10fd47303181c04c220962615f4eb2499cef
Sonia <andrebreijao@gmail.com>
2021-06-04 17:18:45 -0300

    Nova client secret e-mail gmail

--- backend/utils/sendOrderEmail.js
+++ backend/utils/sendOrderEmail.js
@@ -1,13 +1,12 @@
 import nodemailer from 'nodemailer';
 import google from 'googleapis';
-//import { gmail } from 'googleapis/build/src/apis/gmail';
 
 const CLIENT_ID =
-  '618764626040-7v0g1ahgv2kb40hahkmkn0hjn93guujh.apps.googleusercontent.com';
-const CLIENT_SECRET = '8VOW-GE_SZAf2_6w28dhfLNt';
+  '618764626040-knundjnqf8lbm1bedqb7bjma28aua0m7.apps.googleusercontent.com';
+const CLIENT_SECRET = 'dtJ-zrG9SA9lpAJYgxRp-H91';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN =
-  '1//04weDaWJS-V5kCgYIARAAGAQSNwF-L9IrFhHVoUiYJHxhrHPAHJnn7br0VdWDpvlW7U4zutsjibTnPPRJXfE0xqAp8JSwB5AMopk';
+  '1//04GltpCSzXxCkCgYIARAAGAQSNwF-L9Ir-FPmx0VMRkh50-7qmq61v_cJZm2EjAvLuukuXiKumBeHV7_FVb4M32Av382kddM8psg';
 
 //Generate new access token
 const oAuth2Client = new google.Auth.OAuth2Client(
@@ -36,7 +35,7 @@ const sendMail = async (nome, listaItems, email, valorPedido) => {
 
     const mailOptions = {
       from: 'Pedidos Loby <pedidos@loby.com.br>',
-      to: [email, 'andrebreijao@gmail.com'],
+      to: email,
       subject: 'Teste de email',
       text: ` Olá ${nome}, seu pedido foi confirmado! Valor do pedido: ${valorPedido} ${listaItems} Obrigado! `,
       html: ` <h3>Olá ${nome}, seu pedido foi confirmado!</h3> Valor do pedido: ${valorPedido}  <br>${listaItems}<br> Obrigado! `,
@@ -52,6 +51,11 @@ const sendMail = async (nome, listaItems, email, valorPedido) => {
 
 export default sendMail;
 
-// sendMail()
-//   .then((result) => console.log('Email enviado...', result))
-//   .catch((error) => console.log(error.message));
+sendMail(
+  'andre',
+  'lista',
+  ['andrebreijao@gmail.com', 'andrebreijao@hotmail.com'],
+  'R$ 100,20'
+)
+  .then((result) => console.log('Email enviado...', result))
+  .catch((error) => console.log(error.message));
