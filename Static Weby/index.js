5fd84b45b93900168d859f9925ac352844979bcd
sor-andre <andre.breijao@soreto.com>
2021-10-14 11:34:16 -0300

    old data deletion

--- Static Weby/index.js
+++
@@ -1,8 +0,0 @@
-function togglePopup1(){
-    document.getElementById("popup-1").classList.toggle("active");
-  }
-      
-function togglePopup2(){
-    document.getElementById("popup-2").classList.toggle("active");
-  }
-      
